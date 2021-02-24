9a4453e190b78d8a5e6edc5ea6147ac4759debda
Simonni <szkiflay@gmal.com>
2021-02-23 21:19:01 -0500

    added JS & text files

---
+++ W1D2DataTypes/dataTypes.js
@@ -0,0 +1,28 @@
+const prompt = require("prompt-sync")();
+
+//volume in quarts then converts that value in liters.
+const quart = prompt("Enter Volume in Quarts: ");
+const liter = Number(quart) * 0.94
+console.log(liter);
+
+// distance in kilometers and then convert that value in miles
+const km = prompt("Enter Volume in kilometers: ");
+const mile = Number(km) * 0.62
+console.log(mile);
+
+//boxes of cans in stacks
+
+//mileage of a vehicle
+const InitialMlg = prompt("Enter Initial Mileage of your Vehicle: ");
+const FinalMlg = prompt("Enter final Mileage of your Vehicle: ");
+const gallonUsed = prompt("Enter total gallon used: ");
+const MilePerGallon = gallonUsed/(InitialMlg - FinalMlg)
+console.log(Math.abs(MilePerGallon).toFixed(2)+"lt/mile");
+
+//slowest and fastest of your heart rate
+const age = prompt("What is your age? ");
+const maxHBeat = 220- Number(age);
+const lowestHBeat = maxHBeat*65/100;
+const highestHBeat = maxHBeat*85/100;
+console.log(`Your Lowest heart rate is ${lowestHBeat} and Highest is ${highestHBeat}`)
+
