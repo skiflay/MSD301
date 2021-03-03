fbd86228d46fd865be539be72e57b085ee64116a
Simonni <szkiflay@gmal.com>
2021-03-03 18:01:30 -0500

    added & modified

--- W2D3CodeQuality/ClassNotes.js
+++ W2D3CodeQuality/ClassNotes.js
@@ -1,16 +1,6 @@
   
-/* 
-- Alt + Shift + F => for vertical indentation
+/*- Alt + Shift + F => for vertical indentation
 - Use ESLint  -- install it 
 - refactor test is better b/c it will test cases seperatly 
-*/
+ */
 
-function pow(x, n) {
-    let result = 1;
-  
-    for (let i = 0; i < n; i++) {
-      result *= x;
-    }
-  
-    return result;
-  }
